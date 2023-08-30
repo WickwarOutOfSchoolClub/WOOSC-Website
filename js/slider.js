e5fbbb3a2474317d03beefd546c9f949fb577407
T-M-Coates <T-M-Coates@users.noreply.github.com>
2023-08-30 09:52:11 +0000

    deploy: 021447954f3ff3fe9a0c60abd461a5a39232f0e8

--- js/slider.js
+++ js/slider.js
@@ -2,15 +2,25 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
     let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1
     }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 10000);
+    slides[slideIndex - 1].style.left = "0";
+    setTimeout(hideSlides, 800);
+}
+
+function hideSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < slides.length; i++) {
+        if (i !== slideIndex - 1) {
+            slides[i].style.left = "110vw";
+        }
+    }
+    setTimeout(showSlides, 5000);
 }
